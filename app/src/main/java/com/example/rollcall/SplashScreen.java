f06e4ac659333b7c1d671ac752f0123910e4e27f
ariokan <okan_ari@windowslive.com>
2020-10-08 01:55:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rollcall/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.rollcall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
