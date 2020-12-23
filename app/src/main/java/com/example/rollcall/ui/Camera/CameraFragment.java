a10345da5800af97d1999a435e2b6802398b9f55
Yigitcan Guler <guleryigitcan@gmail.com>
2020-12-23 23:06:22 +0300

    user id ye göre klasöre kaydetme

--- app/src/main/java/com/example/rollcall/ui/Camera/CameraFragment.java
+++ app/src/main/java/com/example/rollcall/ui/Camera/CameraFragment.java
@@ -157,7 +157,7 @@ public class CameraFragment extends Fragment {
             progressDialog.show();
             String user_id=mAuth.getCurrentUser().getUid();
             // Defining the child of storageReference
-            StorageReference ref = storageReference.child("images/" + user_id);
+            StorageReference ref = storageReference.child("images/" + user_id).child(UUID.randomUUID().toString());
 
             // adding listeners on upload
             // or failure of image
